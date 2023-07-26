808040b1d3b1dcd778ca158b6ab0c932275826da
alex-romo <alex@nebulosastudio.com>
2023-07-25 23:43:46 -0700

    feat: style improvements and generic text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -41,8 +41,8 @@ const Footer = () => {
   ]
 
   return <section className="flex flex-col items-center py-20 bg-black">
-    <h2>LVMH</h2>
-    <div className="flex gap-4">Follow LVMH on:
+    <h2 className="mb-4">Lorem</h2>
+    <div className="flex gap-4">Follow Lorem on:
       {sm.map(n => <a key={n.title} href={n.href} title={n.title}><Image src={n.img} alt={n.title} width={30} height={30} /></a>)}
     </div>
   </section>
