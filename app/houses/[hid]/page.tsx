773729bf07d3fa098b750b8995e2d433de09ab92
alex-romo <alex@nebulosastudio.com>
2023-08-03 14:56:38 -0700

    feat: finished /houses/[hid].tsx template and lib/ function to serve data

--- app/houses/[hid]/page.tsx
+++ app/houses/[hid]/page.tsx
@@ -1,5 +1,5 @@
-import getHouse from "@/app/lib/getHouseInfo";
-
+import getHouse from "@/app/lib/getHouse";
+import Image from "next/image";
 
 interface Params {
   params: {
@@ -10,13 +10,42 @@ interface Params {
 const House = ({ params: { hid }}: Params) => {
   const house = getHouse(hid);
   return <main>
-    {house ? <section>
-      <div>
-        <h1>{house.title}</h1>
-        <hr />
-        <p>house.desc</p>
-      </div>
-    </section> :
+    {house ? <>
+      <section style={{ backgroundImage: `url('${house.heroBg})` }} className='bg-cover w-screen h-screen flex flex-col justify-center text-center items-center'>
+        <h1 className='text-white'>{house.title}</h1>
+        <hr className="mb-12 mt-6 w-12 border-white"/>
+        <p className='text-white w-5/6 md:w-1/3'>{house.desc}</p>
+      </section>
+      <section className='bg-white flex flex-col items-center py-20 justify-center'>
+        <p className="w-5/6 md:w-1/2 text-xl pb-16 text-center font-semibold">{house.quote}</p>
+        <h3 className="pb-20">{house.author}</h3>
+        <div className="w-5/6 flex flex-wrap md:w-2/3">
+          {house.brands.map(brand => {
+            return <div key={brand.title} className='basis-full md:basis-1/3 p-5 text-center'>
+              <div className="h-56 relative mb-5">
+                <Image src={brand.img} alt={brand.title} fill />
+              </div>
+              <h3>{brand.title}</h3>
+              <h4>{brand.date}</h4>
+            </div>
+          })}
+        </div>
+      </section>
+      <section className="bg-secondary flex flex-col items-center py-20 justify-center">
+        <h2 className="mb-5">{house.otherSectors.title}</h2>
+        <div className="w-5/6 flex flex-wrap md:w-2/3">
+          {house.otherSectors.brands.map(brand => {
+            return <div key={brand.title} className='basis-full md:basis-1/3 p-5 text-center'>
+              <div className="h-56 relative mb-5">
+                <Image src={brand.img} alt={brand.title} fill />
+              </div>
+              <h3>{brand.title}</h3>
+              <h4>{brand.date}</h4>
+            </div>
+          })}
+        </div>
+      </section>
+    </>:
     <div>Nothing to see here</div>}
   </main>
 }
--- app/lib/getHouse.tsx
+++ app/lib/getHouse.tsx
@@ -2,32 +2,270 @@ import { ObjectType } from "typescript";
 
 const data = {
   wines: {
+    heroBg: 'https://loremflickr.com/1000/720/newyork',
     title: 'Wines & Spirits',
-    desc: ''
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    quote: 'empor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    author: 'sit voluptatem accusantium natus erro',
+    brands: [
+      {
+        title: 'e velit esse cillum dol',
+        date: 'Since 1234',
+        img: 'https://loremflickr.com/400/300/newyork',
+      },
+      {
+        title: 'e velit esse cillum dol',
+        date: 'Since 1234',
+        img: 'https://loremflickr.com/400/300/newyork',
+      },
+      {
+        title: 'e velit esse cillum dol',
+        date: 'Since 1234',
+        img: 'https://loremflickr.com/400/300/newyork',
+      },
+      {
+        title: 'e velit esse cillum dol',
+        date: 'Since 1234',
+        img: 'https://loremflickr.com/400/300/newyork',
+      },
+      {
+        title: 'e velit esse cillum dol',
+        date: 'Since 1234',
+        img: 'https://loremflickr.com/400/300/newyork',
+      },
+      {
+        title: 'e velit esse cillum dol',
+        date: 'Since 1234',
+        img: 'https://loremflickr.com/400/300/newyork',
+      },
+    ],
+    otherSectors: {
+      title: 'Other Sectors',
+      brands: [
+        {
+          title: 'e velit esse cillum dol',
+          date: 'Since 1234',
+          img: 'https://loremflickr.com/400/300/newyork',
+        },
+        {
+          title: 'e velit esse cillum dol',
+          date: 'Since 1234',
+          img: 'https://loremflickr.com/400/300/newyork',
+        },
+        {
+          title: 'e velit esse cillum dol',
+          date: 'Since 1234',
+          img: 'https://loremflickr.com/400/300/newyork',
+        },
+        {
+          title: 'e velit esse cillum dol',
+          date: 'Since 1234',
+          img: 'https://loremflickr.com/400/300/newyork',
+        },
+        {
+          title: 'e velit esse cillum dol',
+          date: 'Since 1234',
+          img: 'https://loremflickr.com/400/300/newyork',
+        },
+        {
+          title: 'e velit esse cillum dol',
+          date: 'Since 1234',
+          img: 'https://loremflickr.com/400/300/newyork',
+        },
+      ]
+    }
   },
   retailing: {
-    title: 'Retailing'
+    title: 'Retailing',
+    heroBg: '',
+    desc: '',
+    quote: '',
+    author: '',
+    brands: [
+      {
+        title: '',
+        date: '',
+        img: '',
+      },
+    ],
+    otherSectors: {
+      title: 'Other Sectors',
+      brands: [
+        {
+          title: '',
+          date: '',
+          img: '',
+        },
+      ]
+    }
   },
   fragance: {
-    title: 'Fragance & Cosmetics'
+    title: 'Fragance & Cosmetics',
+    heroBg: '',
+    desc: '',
+    quote: '',
+    author: '',
+    brands: [
+      {
+        title: '',
+        date: '',
+        img: '',
+      },
+    ],
+    otherSectors: {
+      title: 'Other Sectors',
+      brands: [
+        {
+          title: '',
+          date: '',
+          img: '',
+        },
+      ]
+    }
   },
   fashion: {
-    title: 'Fashion & Interiors'
+    title: 'Fashion & Interiors',
+    heroBg: '',
+    desc: '',
+    quote: '',
+    author: '',
+    brands: [
+      {
+        title: '',
+        date: '',
+        img: '',
+      },
+    ],
+    otherSectors: {
+      title: 'Other Sectors',
+      brands: [
+        {
+          title: '',
+          date: '',
+          img: '',
+        },
+      ]
+    }
   },
   realEstate: {
-    title: 'Real Estate & Hospitality'
+    title: 'Real Estate & Hospitality',
+    heroBg: '',
+    desc: '',
+    quote: '',
+    author: '',
+    brands: [
+      {
+        title: '',
+        date: '',
+        img: '',
+      },
+    ],
+    otherSectors: {
+      title: 'Other Sectors',
+      brands: [
+        {
+          title: '',
+          date: '',
+          img: '',
+        },
+      ]
+    }
   },
   media: {
-    title: 'Media'
+    title: 'Media',
+    heroBg: '',
+    desc: '',
+    quote: '',
+    author: '',
+    brands: [
+      {
+        title: '',
+        date: '',
+        img: '',
+      },
+    ],
+    otherSectors: {
+      title: 'Other Sectors',
+      brands: [
+        {
+          title: '',
+          date: '',
+          img: '',
+        },
+      ]
+    }
   },
   distribution: {
-    title: 'Distribution'
+    title: 'Distribution',
+    heroBg: '',
+    desc: '',
+    quote: '',
+    author: '',
+    brands: [
+      {
+        title: '',
+        date: '',
+        img: '',
+      },
+    ],
+    otherSectors: {
+      title: 'Other Sectors',
+      brands: [
+        {
+          title: '',
+          date: '',
+          img: '',
+        },
+      ]
+    }
   },
   tobacco: {
-    title: 'Tobacco'
+    title: 'Tobacco',
+    heroBg: '',
+    desc: '',
+    quote: '',
+    author: '',
+    brands: [
+      {
+        title: '',
+        date: '',
+        img: '',
+      },
+    ],
+    otherSectors: {
+      title: 'Other Sectors',
+      brands: [
+        {
+          title: '',
+          date: '',
+          img: '',
+        },
+      ]
+    }
   },
   other: {
-    title: 'Other Activities'
+    title: 'Other Activities',
+    heroBg: '',
+    desc: '',
+    quote: '',
+    author: '',
+    brands: [
+      {
+        title: '',
+        date: '',
+        img: '',
+      },
+    ],
+    otherSectors: {
+      title: 'Other Sectors',
+      brands: [
+        {
+          title: '',
+          date: '',
+          img: '',
+        },
+      ]
+    }
   }
 }
 
